350a7ad311a9ec6fdf2273643d6705e3ef495d6e
makmakkerti <makmakkerti007@gmail.com>
2020-05-03 20:25:51 +0300

    progression game added

---
+++ bin/brain-progression.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// eslint-disable-next-line import/extensions
+import { game } from '../src/index.js';
+import progressionGame from '../src/games/progression.js';
+
+game(progressionGame);
---
+++ src/games/progression.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-console */
+import readlineSync from 'readline-sync';
+import {
+  greeting, askQuestion, wrongAnswer, winner, generateRandomNumber, log,
+} from '../index.js';
+
+const progressionLength = 10;
+const progressionArrayGenerator = (startNumber) => {
+  const step = Math.round((Math.random() * 10));
+  let temp = startNumber;
+  const arr = [];
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    arr.push(temp);
+    temp += step;
+  }
+  return arr;
+};
+
+const progressionFromArray = (arr) => {
+  const hidePosition = Math.round((Math.random() * 10));
+  const result = [];
+  let progressionString = '';
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    if (i === hidePosition) {
+      progressionString += '.. ';
+      result.push(arr[i]);
+    } else {
+      progressionString += `${arr[i]} `;
+    }
+  }
+  result.push(progressionString);
+  return result;
+};
+
+const progressionGame = (counter, username) => {
+  const description = 'What number is missing in the progression?';
+  if (counter === 3) return winner();
+  if (counter === 0) greeting(description);
+
+  const generatedNumber = generateRandomNumber();
+  const arrayProgression = progressionArrayGenerator(generatedNumber);
+  const progression = progressionFromArray(arrayProgression);
+  askQuestion(`${progression[1]}`);
+
+  const answer = readlineSync.question('Your answer: ');
+  const correctAnswer = progression[0];
+
+  // eslint-disable-next-line no-mixed-operators
+  if (+answer === correctAnswer) {
+    log('Correct!');
+    return progressionGame(counter + 1, username);
+  }
+  return wrongAnswer(answer, correctAnswer);
+};
+
+export default progressionGame;
